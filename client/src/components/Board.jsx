eb24e6825af7bbb3bb121b409238324dea36674c
vasyl-n <vasyl.nrk@gmail.com>
2018-03-25 00:53:59 -0700

    setting current piece coordinates

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -9,7 +9,11 @@ class Board extends React.Component {
     this.canMoveDown = () => {};
     this.rotatePiece = () => {};
     this.moveDown = () => {};
+    this.setCurrentPieceCoordinates = (x,y) => {
+        console.log(x, y)
+    };
     this.state = {
+      currentPieceCoordinates: [[],[],[],[]],
       piece: this.props.piece.slice(),
       board: [
         [0,0,0,0,0,0,0,0,0,0],
@@ -37,29 +41,11 @@ class Board extends React.Component {
 
     this.placePiece = () => {
       var board = this.state.board.slice();
-      // board[0][3] = Number(this.state.piece[0][0]);
-      // board[0][4] = Number(this.state.piece[0][1]);
-      // board[0][5] = Number(this.state.piece[0][2]);
-      // board[0][6] = Number(this.state.piece[0][3]);
-
-      // board[1][3] = Number(this.state.piece[1][0]);
-      // board[1][4] = Number(this.state.piece[1][1]);
-      // board[1][5] = Number(this.state.piece[1][2]);
-      // board[1][6] = Number(this.state.piece[1][3]);
-
-      // board[2][3] = Number(this.state.piece[2][0]);
-      // board[2][4] = Number(this.state.piece[2][1]);
-      // board[2][5] = Number(this.state.piece[2][2]);
-      // board[2][6] = Number(this.state.piece[2][3]);
-
-      // board[3][3] = Number(this.state.piece[3][0]);
-      // board[3][4] = Number(this.state.piece[3][1]);
-      // board[3][5] = Number(this.state.piece[3][2]);
-      // board[3][6] = Number(this.state.piece[3][3]);
       for( var i = 0; i <= 3; i++ ) {
         var a = 3;
         for ( var j = 0; j < 4; j++ ) {
           board[i][a] = Number(this.state.piece[i][j]);
+          if(board[i][a] === 1) this.setCurrentPieceCoordinates(i, a)
           a++;
         }
       }
